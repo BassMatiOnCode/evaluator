093855162a8f024016dc71809c3d7cc07b7bbc83
bassmationcode <BassmatiOnCode@freenet.de>
2023-05-27 14:54:54 +0200

    Function-based version, before converting to class-based design.

--- docs/evaluator-1.js
+++ docs/evaluator-1.js
@@ -1,25 +1,39 @@
-//		evaluator-1.js    2023-05-22    usp
+//		evaluator-1.js    2023-05-27    usp
+
+export class evaluator {
+	constructor ( options = { } ) {
+		if ( options.container ) {
+			this.container = options.container ;
+			if ( typof options.container === "string" ) this.container = document.getElementById( options.container ) ;
+			else this.container = options.container;
+			}
+		else this.container = document;
+		}
+	}
 
 	// Bindings associate variables with elements.
 export const updateBindings = { } ;
 export const functionBindings = { } ;
+var scope ;
 
 export function set ( name, value ) {
 	//// Sets a variable and updates bound properties.
 		// Bail out if nothing to do.
-	if ( globalThis[ name ] === value ) return ;
+	if ( scope[ name ] === value ) return value ;
 		// Set the variable
-	globalThis[ name ] = value ;
+	scope[ name ] = value ;
+	console.info ( `evaluator:set(): ${name} = ${value}` );
 		// Update bound properties
 	let bindings = updateBindings[ name ] || [ ] ;
 	for ( let binding of bindings ) binding.object [ binding.property ] = value ;
 		// Call bound functions
 	bindings = functionBindings[ name ] || [ ] ;
 	for ( const fn of bindings ) {
-		try { fn.call( ); }
+		try { fn.call( scope ); }
 			// Errors logged but don't break script execution
 		catch( e ){ console.log( e ); }
 		}
+	return value;
 	}
 export function inputChangeHandler( evt ) {
 		// Get the bound object
@@ -27,13 +41,13 @@ export function inputChangeHandler( evt ) {
 	const value = evt.target.hasAttribute( "data-attrib" ) ? 
 		evt.target.getAttribute( evt.target.getAttribute( "data-attrib" )) : 
 		evt.target[ evt.target.getAttribute( "data-member" )] ;
-	console.log ( `${name} = ${value}` );
 		// Set its value
 	set( name, value );
 	}
 export function addFunctionBindings ( name, functions ) {
 	////	Binds a list of functions to a variable name
 		// Make sure that functions is an array of functions
+	if ( ! functions ) return; 
 	if ( typeof functions === "string" ) functions = functions.split( /\s*,\s*/ ) ;
 		// Get the function bindings for the variable name
 	const bindings = functionBindings[ name ] || (functionBindings[ name ] = [ ] );
@@ -42,28 +56,34 @@ export function addFunctionBindings ( name, functions ) {
 		addCallBinding : {
 				// Prevent duplicate function bindings
 			for ( const binding of bindings ) if ( binding.name === fn ) break addCallBinding ;
-				// Add the new function call binding.
-			bindings.push( globalThis[ fn ] );
+				// Add the new function call binding
+			console.info( `evaluator:addFunctionBindings: ${name}, ${fn}` );
+			bindings.push( scope[ fn ] );
 	}	}	}
-export function addPropertyUpdateBinding ( name, objectReference, propertyName ) {
+export function addPropertyBinding ( name, objectReference, propertyName ) {
 		// Get the binding for the named variable
 	const bindings = updateBindings[ name ] || ( updateBindings[ name ] = [ ] );
 		// Prevent multiple references to the same target.
 	for ( const binding of bindings ) if ( binding.object === objectReference && binding.property === propertyName ) return ;
 		// Add the element reference to the binding.
 	bindings.push( { object : objectReference, property : propertyName } );
+	console.info( `evaluator:addPropertyBinding: ${name}, ${objectReference.id || objectReference.name || (objectReference.getAttribute && objectReference.getAttribute("name")) || objectReference}, ${propertyName}`);
 	}
 export function addElementUpdateBinding( name, element ) {
 	////	Binds an element attribute or property to a variable for updating
 	if ( element.hasAttribute( "data-member" )) {
-		addPropertyUpdateBinding( name, element, element.getAttribute( "data-member" ));
+		addPropertyBinding( name, element, element.getAttribute( "data-member" ));
 		}
 	else if ( element.hasAttribute( "data-attrib" )) {
-		addPropertyUpdateBinding( name, element.attributes[ element.getAttribute( "data-attrib" )], "value" ) ;
+		addPropertyBinding( name, element.attributes[ element.getAttribute( "data-attrib" )], "value" ) ;
 		}
 	else throw new Error( "evaluator.js:initPage(): No update attribute or property specified." );  // TODO: Decide if we can exit silently or throw an error here. 
 	}
-export function initPage ( container=document ) {
+export function initPage ( options = { } ) {
+		// Process the options
+	scope = options.scope || globalThis ;
+	if ( typeof options.container === "string" ) options.container = document.getElementById( options.container );
+	const container = options.container || document ;
 		// Bind read-only elements to a variable
 	container.querySelectorAll( "[data-get]" )
 		.forEach( element => addElementUpdateBinding( element.getAttribute( "data-get" ), element )) ;
